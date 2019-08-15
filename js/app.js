6d72c4d9cf5e5c402277e46a7760cf27183461ea
Ege Uz <enkomaun@gmail.com>
2019-08-15 18:41:43 -0400

    will it work?

---
+++ js/app.js
@@ -0,0 +1,61 @@
+//HANDLE AUDIO RECORDING
+const recordAudio = () =>
+  new Promise(async resolve => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    const audioChunks = [];
+    mediaRecorder.addEventListener("dataavailable", event => {
+      audioChunks.push(event.data);
+    });
+    const start = () => mediaRecorder.start();
+    const stop = () =>
+      new Promise(resolve => {
+        mediaRecorder.addEventListener("stop", () => {
+          const audioBlob = new Blob(audioChunks);
+          const audioUrl = URL.createObjectURL(audioBlob);
+          const audio = new Audio(audioUrl);
+          const play = () => audio.play();
+          resolve({ audioBlob, audioUrl, play });
+        });
+        mediaRecorder.stop();
+      });
+    resolve({ start, stop });
+  });
+let recorder = null;  
+let audio = null;
+const recordStop = async () => {
+  if (recorder) {
+    audio = await recorder.stop();
+    recorder = null;
+    document.querySelector("#record-stop-button").classList.remove('recording');
+    document.querySelector("#play-audio-button").disabled = false;
+  } else {
+    recorder = await recordAudio();
+    recorder.start();
+    document.querySelector("#record-stop-button").classList.add('recording');
+    document.querySelector("#play-audio-button").disabled = true;
+  }
+};
+const playAudio = () => {
+  if (audio && typeof audio.play === "function") {
+    audio.play();
+  }
+};
+
+// while (!audio.paused) {
+//   document.querySelector("#play-audio-button").disabled = true;
+// }
+
+//HANDLE QUESTIONS
+const questionArray = [
+  "Why do people become addicted to things?",
+  "In our globalized world, are nations and borders still necessary?",
+  "Which one is more important, nature or nurture?",
+  "Is the world too populated?",
+  "Lorem ipsum dolor sit amet?",
+  "Consectetur adipiscing elit?"
+];
+
+//Randomly generate a question on launch
+const rng = (min, max) => Math.floor(Math.random() * max - min) + min;
+document.getElementById('question').innerHTML = questionArray[rng(0, questionArray.length-1)];
